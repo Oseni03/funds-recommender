190d78a389779f61881037bec7539df7e045f7c0
Oseni Ayomide <oseniayomide57@gmail.com>
2023-10-19 17:51:50 +0100

    funds: updated profile signal for all post-save

--- pkg/apps/funds/signals.py
+++ pkg/apps/funds/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from django.dispatch import Signal
 from django.dispatch import receiver
 
 from .models import FundProfile, Fund
@@ -7,10 +8,9 @@ from .tasks import get_fund_summary, get_profile_summary
 
 @receiver(post_save, sender=FundProfile)
 def create_profile_summary(sender, instance, created, **kwargs):
-    if created:
-        texts = instance.objectives + "\n" + instance.project_summary + "\n" + instance.background
-        
-        get_profile_summary.delay(texts)
+    texts = instance.objectives + "\n" + instance.project_summary + "\n" + instance.background
+    
+    get_profile_summary.delay(instance.id, texts)
 
 
 @receiver(post_save, sender=Fund)
